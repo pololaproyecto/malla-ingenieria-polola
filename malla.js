904dd5e7c01b5f9cd44fd7b85d35aa6744b42426
pololaproyecto <ithan.barahona.v@mail.pucv.cl>
2025-07-15 19:58:29 -0400

    Update malla.js

--- malla.js
+++ malla.js
@@ -1,1 +1,45 @@
+const ramos = [
+  { codigo: "MAT101", nombre: "Cálculo I", prerequisitos: [] },
+  { codigo: "MAT102", nombre: "Cálculo II", prerequisitos: ["MAT101"] },
+  { codigo: "FIS101", nombre: "Física I", prerequisitos: [] },
+  { codigo: "FIS102", nombre: "Física II", prerequisitos: ["FIS101", "MAT101"] },
+  { codigo: "IIN101", nombre: "Intro a la Ing.", prerequisitos: [] },
+  { codigo: "ECO101", nombre: "Microeconomía", prerequisitos: [] },
+  { codigo: "ECO102", nombre: "Macroeconomía", prerequisitos: ["ECO101"] },
+];
 
+const mallaDiv = document.getElementById("malla");
+const estado = {};
+
+function actualizarEstados() {
+  ramos.forEach((ramo) => {
+    const elem = document.getElementById(ramo.codigo);
+    const cursado = estado[ramo.codigo];
+    const habilitado = !cursado && ramo.prerequisitos.every(pr => estado[pr]);
+
+    elem.className = "ramo";
+    if (cursado) {
+      elem.classList.add("cursado");
+    } else if (habilitado) {
+      elem.classList.add("habilitado");
+    }
+
+    elem.onclick = () => {
+      estado[ramo.codigo] = !estado[ramo.codigo];
+      actualizarEstados();
+    };
+  });
+}
+
+function dibujarMalla() {
+  ramos.forEach((ramo) => {
+    const div = document.createElement("div");
+    div.className = "ramo";
+    div.id = ramo.codigo;
+    div.innerHTML = `<strong>${ramo.nombre}</strong><br><small>${ramo.codigo}</small>`;
+    mallaDiv.appendChild(div);
+  });
+  actualizarEstados();
+}
+
+dibujarMalla();
